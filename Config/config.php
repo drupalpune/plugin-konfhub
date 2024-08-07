04b98e5b1e75c87f08b55144666142cdf14b11f3
Rahul Shinde <shinde.r.a@gmail.com>
2024-08-07 18:52:10 +0530

    Added Webhook listener and coverage for the same

--- Config/config.php
+++ Config/config.php
@@ -7,4 +7,14 @@ return [
     'description' => 'Integrate KonfHub with Mautic to nurture the leads and enable personalized communication.',
     'version'     => '1.0.0',
     'author'      => 'Rahul Shinde',
+    'routes'      => [
+        'public' => [
+            'mautic_konfhub_webhook' => [
+                'path'       => '/plugin/webhook/konfhub',
+                'controller' => 'MauticPlugin\KonfhubBundle\Controller\WebhookController::listenAction',
+                'method'     => 'POST',
+                'format'     => 'json',
+            ],
+        ],
+    ],
 ];
